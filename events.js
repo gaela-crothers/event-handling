0b630459ed07986e3cac127c8b34fd67f2d71419
gaela-crothers <gaelabc@hotmail.com>
2016-10-10 14:48:53 +1300

    add fucntion three

--- events.js
+++ events.js
@@ -8,6 +8,7 @@ function start () {
   // when you mouse over it.
   one()
   two()
+  three()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -35,7 +36,15 @@ function two () {
   two.addEventListener('mouseleave', makeWhite)
 }
 // CREATE FUNCTION three HERE
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
 
+  // Next, we add an event listener to it:
+  three.addEventListener('mouseenter', makeOrange)
+
+  // Finally, we add one to make the colour white again
+  three.addEventListener('mouseleave', makeWhite)
 // CREATE FUNCTION four HERE
 
 // Changes the background color of event's target
@@ -49,4 +58,8 @@ function makeWhite (evt) {
 
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green'
+}
+
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
